eeca9aa8a55373a75b19ed54f1ef178d3e7c45ed
Sara Stec <sara.stec1002@gmail.com>
2024-08-22 11:03:15 +0200

    Update images and links

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,14 +4,14 @@
         <header>
             <nav>
                 <div class="text-6xl p-8 flex justify-center font-normal">
-                    <NuxtLink to="/">Sara Stec</NuxtLink>
+                    <RouterLink to="/">Sara Stec</RouterLink>
                 </div>
                 <div class="navtab text-xl flex justify-center space-x-11 p-5 w-full text-zinc-500">
                     <hr class="w-64 h-px my-4 border-0 dark:bg-zinc-500">
-                    <NuxtLink to="/" class="hover:text-black">About</NuxtLink>
-                    <NuxtLink to="/projects/quanta" class="hover:text-black">Projects</NuxtLink>
-                    <NuxtLink to="/contact" class="hover:text-black">Contact</NuxtLink>
-                    <NuxtLink to="/resume" class="hover:text-black">Resume</NuxtLink>
+                    <RouterLink to="/" class="hover:text-black">About</RouterLink>
+                    <RouterLink to="/projects/quanta" class="hover:text-black">Projects</RouterLink>
+                    <RouterLink to="/contact" class="hover:text-black">Contact</RouterLink>
+                    <RouterLink to="/resume" class="hover:text-black">Resume</RouterLink>
                     <hr class="w-64 h-px my-4 border-0 dark:bg-zinc-500">
                 </div>
             </nav>
--- layouts/projects.vue
+++ layouts/projects.vue
@@ -4,14 +4,14 @@
         <header>
             <nav>
                 <div class="text-6xl p-8 flex justify-center font-normal">
-                    <NuxtLink to="/">Sara Stec</NuxtLink>
+                    <RouterLink to="/">Sara Stec</RouterLink>
                 </div>
                 <div class="navtab text-xl flex justify-center space-x-11 p-5 w-full text-zinc-500">
                     <hr class="w-64 h-px my-4 border-0 dark:bg-zinc-500">
-                    <NuxtLink to="/" class="hover:text-black">About</NuxtLink>
-                    <NuxtLink to="/projects/quanta" class="projects hover:text-black">Projects</NuxtLink>
-                    <NuxtLink to="/contact" class="hover:text-black">Contact</NuxtLink>
-                    <NuxtLink to="/resume" class="hover:text-black">Resume</NuxtLink>
+                    <RouterLink to="/" class="hover:text-black">About</RouterLink>
+                    <RouterLink to="/projects/quanta" class="projects hover:text-black">Projects</RouterLink>
+                    <RouterLink to="/contact" class="hover:text-black">Contact</RouterLink>
+                    <RouterLink to="/resume" class="hover:text-black">Resume</RouterLink>
                     <hr class="w-64 h-px my-4 border-0 dark:bg-zinc-500">
                 </div>
             </nav>
@@ -19,20 +19,20 @@
         <div class="flex mx-6">
             <div class="project-tab grid-cols-1 text-zinc-500">
                 <div class="tab hover:text-black">
-                    <NuxtLink to="/projects/quanta" class="text-xl">Quanta</NuxtLink><br>
-                    <NuxtLink to="/projects/quanta">A time management web app team project.</NuxtLink>
+                    <RouterLink to="/projects/quanta" class="text-xl">Quanta</RouterLink><br>
+                    <RouterLink to="/projects/quanta">A time management web app team project.</RouterLink>
                 </div>
                 <div class="tab hover:text-black">
-                    <NuxtLink to="/projects/slimebeats" class="text-xl">SlimeBeats</NuxtLink><br>
-                    <NuxtLink to="/projects/slimebeats">A small rhythm game made in Unity.</NuxtLink>
+                    <RouterLink to="/projects/slimebeats" class="text-xl">SlimeBeats</RouterLink><br>
+                    <RouterLink to="/projects/slimebeats">A small rhythm game made in Unity.</RouterLink>
                 </div>
                 <div class="tab hover:text-black">
-                    <NuxtLink to="/projects/personal_website" class="text-xl">Personal Website</NuxtLink><br>
-                    <NuxtLink to="/projects/personal_website">The website you're looking at right now!</NuxtLink>
+                    <RouterLink to="/projects/personal_website" class="text-xl">Personal Website</RouterLink><br>
+                    <RouterLink to="/projects/personal_website">The website you're looking at right now!</RouterLink>
                 </div>
                 <div class="tab hover:text-black">
-                    <NuxtLink to="/projects/assembly_music_player" class="text-xl">Assembly Music Player</NuxtLink><br>
-                    <NuxtLink to="/projects/assembly_music_player">An Assembly app able to play some video game OST.</NuxtLink>
+                    <RouterLink to="/projects/assembly_music_player" class="text-xl">Assembly Music Player</RouterLink><br>
+                    <RouterLink to="/projects/assembly_music_player">An Assembly app able to play some video game OST.</RouterLink>
                 </div>
             </div>
             <div class="inside pl-14 pt-4 mb-6">
